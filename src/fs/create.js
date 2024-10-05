9073f14e4c2908564f33206c4c2cd02143f17813
Vaverka123 <maslova.vera.2019@yandex.by>
2024-10-05 16:51:03 +0200

    feat: solve create.js

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,24 @@
+const fs = require("fs");
+const path = require("path");
+
 const create = async () => {
-    // Write your code here 
+  const filePath = path.join(__dirname, "files", "fresh.txt");
+  const content = "I am fresh and young";
+  fs.access(filePath, fs.constants.F_OK, (err) => {
+    if (!err) {
+      // File exists, throw error
+      throw new Error("FS operation failed");
+    } else {
+      // File doesn't exist, create the file
+      fs.writeFile(filePath, content, (err) => {
+        if (err) {
+          console.error("Error writing file:", err);
+          return;
+        }
+        console.log("File created successfully!");
+      });
+    }
+  });
 };
 
 await create();
