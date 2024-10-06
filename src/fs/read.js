12c563c86452dc75fe28ea5fd167792fa5a32319
Vaverka123 <maslova.vera.2019@yandex.by>
2024-10-06 15:21:19 +0200

    feat: solve read.js

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,22 @@
+import fs from "fs/promises";
+import path from "path";
+
 const read = async () => {
-    // Write your code here 
+  const filePath = path.join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "fileToRead.txt"
+  );
+
+  try {
+    await fs.access(filePath);
+    const content = await fs.readFile(filePath, "utf-8");
+    console.log(content);
+  } catch {
+    console.log("FS operation failed");
+  }
 };
 
 await read();
