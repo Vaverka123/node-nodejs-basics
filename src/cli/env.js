d472778c1e4d56265b3ef1c50b07f0f83d659de7
Vaverka123 <maslova.vera.2019@yandex.by>
2024-10-06 20:34:22 +0200

    feat: solve cli.env

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,12 @@
 const parseEnv = () => {
-    // Write your code here 
+  const variables = [];
+
+  for (const [key, value] of Object.entries(process.env)) {
+    if (key.startsWith("RSS_")) {
+      variables.push(`${key}=${value}`);
+    }
+  }
+  console.log(variables.join("; "));
 };
 
 parseEnv();
