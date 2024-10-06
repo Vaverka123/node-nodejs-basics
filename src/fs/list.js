2516884a96992f1a1242b3b9b7df01316d772911
Vaverka123 <maslova.vera.2019@yandex.by>
2024-10-06 15:04:37 +0200

    feat: solve list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,21 @@
+import fs from "fs/promises";
+import path from "path";
+
 const list = async () => {
-    // Write your code here 
+  const dirPath = path.join(process.cwd(), "src", "fs", "files");
+
+  try {
+    const fileNames = await fs.readdir(dirPath);
+    (await fileNames).forEach((file) => {
+      console.log(file);
+    });
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      throw new Error("FS operation failed");
+    } else {
+      throw error;
+    }
+  }
 };
 
 await list();
