01538c2df49258aa9007e96f0996fc6651278647
Vaverka123 <maslova.vera.2019@yandex.by>
2024-10-06 13:12:01 +0200

    feat: solve rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,37 @@
+import fs from "fs/promises";
+import path from "path";
+
 const rename = async () => {
-    // Write your code here 
+  const wrongName = path.join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "wrongFilename.txt"
+  );
+  const properName = path.join(
+    process.cwd(),
+    "src",
+    "fs",
+    "files",
+    "properFilename.md"
+  );
+
+  try {
+    (await fs.access(properName)) || !fs.access(wrongName);
+    console.log("FS operation failed");
+  } catch (error) {
+    if (error.code === "ENOENT") {
+      try {
+        await fs.rename(wrongName, properName);
+        console.log("File renamed successfully!");
+      } catch (writeError) {
+        console.log("Error rename file:", writeError);
+      }
+    } else {
+      console.log("Error checking file existence:", error);
+    }
+  }
 };
 
 await rename();
